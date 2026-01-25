33760f968c7efb8569d0e2a4a63bae73e6f106fa
Aman <ak4440204@gmail.com>
2026-01-25 14:14:04 +0530

    Change navbar backgrounds to gray tones and remove harsh border lines

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -133,7 +133,7 @@ export const BottomTabBar = () => {
     return (
         <nav className="fixed bottom-0 left-0 right-0 z-[100] md:hidden">
             {/* Glassmorphic background */}
-            <div className="absolute inset-0 bg-white/90 dark:bg-[#1c1c1e]/95 backdrop-blur-xl backdrop-saturate-150 border-t border-black/[0.04] dark:border-white/[0.06]" />
+            <div className="absolute inset-0 bg-[#f5f5f7]/95 dark:bg-[#2c2c2e]/95 backdrop-blur-xl backdrop-saturate-150" />
 
             {/* Tab container */}
             <div className="relative flex items-center justify-around px-2 pb-safe-bottom pt-2">
